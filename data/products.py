eb284b7e8790a32ceb7138f306ba370fa0cdf2c2
Snezhana <snezhanakorekova_official@mail.ru>
2022-04-29 18:11:22 +0300

    Add files

---
+++ data/products.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Products(SqlAlchemyBase):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    composition = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    path_to_photo = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def __repr__(self):
+        return f'<Colonist> {self.id} "{self.title}" {self.composition} {self.price} {self.path_to_photo}'
---
+++ main.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required
+from data import db_session
+from data.users import User
+from data.products import Products
+from forms.user import RegisterForm, LoginForm
+from data.api import work_with_api
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# получение полного адреса города Дубна и создание изображения по адресу "Дубна"
+full_adress = work_with_api('Дубна', new_picture=True)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form, address=full_adress)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают", address=full_adress)
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть", address=full_adress)
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            phone=form.phone.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+
+    return render_template('register.html', title='Регистрация', form=form, address=full_adress)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    db_session.global_init("db/products.db")
+    db_sess = db_session.create_session()
+    products = []
+    for product in db_sess.query(Products).all():
+        products.append((product.title, product.composition, product.price, product.path_to_photo))
+
+    return render_template('index.html', products=products, address=full_adress)
+
+
+def add_user(surname, name, email, phone):
+    user = User()
+    user.surname = surname
+    user.name = name
+    user.email = email
+    user.phone = phone
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+
+def add_product(title, composition, price, path_to_photo):
+    product = Products()
+    product.title = title
+    product.composition = composition
+    product.price = price
+    product.path_to_photo = path_to_photo
+    db_sess = db_session.create_session()
+    db_sess.add(product)
+    db_sess.commit()
+
+
+def main():
+    # db_session.global_init("db/products.db")
+    # db_sess = db_session.create_session()
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
